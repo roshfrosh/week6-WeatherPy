7416222dbae32ef485cdcdf75c4df5baf9db9c67
roshfrosh <rchainani100@gmail.com>
2019-07-10 00:02:21 -0400

    weatherpy

---
+++ api_keys.py
@@ -0,0 +1,6 @@
+# OpenWeatherMap API Key
+#api_key = "e27ca9f1faa4edfca7378b1af7af9156"
+#api_key = "41e44f2582b81c579373ca42c8eeddae"
+#api_key = "5ed58da866a097dc1b222c34cbd38c03"
+#api_key = "3ad0080cfdc052226971ee5f1d1935f9"
+api_key = "YOUR API KEY HERE"
